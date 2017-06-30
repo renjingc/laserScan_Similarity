d106a8075c41c8ee62bbd7591f929442e8045c76
yh <zjuyh@sina.cn>
2017-06-30 23:29:02 +0800

    AUC

--- src/drawPrs.m
+++ src/drawPrs.m
@@ -1,12 +1,12 @@
-[reca_margin8, prec_margin8] = pr(gnd_inverse, loss);
-plot(reca_margin8, prec_margin8);
+[reca_margin8, prec_margin8] = pr(gnd_inverse, loss_new_2000);
+plot(reca_margin8, prec_margin8, 'LineWidth', 2);
 hold on;
 
-[reca_emd, prec_emd] = pr(gnd_inverse, EMDList);
-plot(reca_emd, prec_emd);
-hold on;
-
-[reca_cos, prec_cos] = pr(gnd, CosList);
-plot(reca_cos, prec_cos);
-
-legend('Siamese', 'EMD', 'Cos');
+% [reca_emd, prec_emd] = pr(gnd_inverse, EMDList);
+% plot(reca_emd, prec_emd, 'LineWidth', 2);
+% hold on;
+% 
+% [reca_cos, prec_cos] = pr(gnd, CosList);
+% plot(reca_cos, prec_cos, 'LineWidth', 2);
+% 
+% legend('Siamese', 'EMD', 'Cos');
---
+++ src/drawROCs.m
@@ -0,0 +1,16 @@
+[X_loss,Y_loss,T_loss,AUC_loss] = perfcurve(gnd_inverse, loss_new_2000, '0');
+plot(X_loss,Y_loss,'LineWidth', 2);
+AUC_loss
+hold on;
+
+[X_emd,Y_emd,T_emd,AUC_emd] = perfcurve(gnd_inverse, EMDList, '0');
+plot(X_emd,Y_emd,'LineWidth', 2);
+AUC_emd
+hold on;
+
+[X_cos,Y_cos,T_cos,AUC_cos] = perfcurve(gnd, CosList, '0');
+plot(X_cos,Y_cos,'LineWidth', 2);
+AUC_cos
+hold on;
+
+legend('Siamese', 'EMD', 'Cos');
