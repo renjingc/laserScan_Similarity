6856736ad80fa98d028aebf4d6594baced09be28
yh <zjuyh@sina.cn>
2017-07-04 22:20:19 +0800

    MAT

--- src/getTrueFalseIndexes_xm.m
+++ src/getTrueFalseIndexes_xm.m
@@ -13,3 +13,27 @@
 % [trueIndex02_5m, falseIndex02_5m] = getTrainIndex(SMTrue02_5m, 1800);
 % [trueIndex02_8m, falseIndex02_8m] = getTrainIndex(SMTrue02_8m, 1000);
 % [trueIndex02_10m, falseIndex02_10m] = getTrainIndex(SMTrue02_10m, 700);
+
+% 05
+% [trueIndex05_1m, falseIndex05_1m] = getTrainIndex(SMTrue05_1m, 2000);
+% [trueIndex05_2m, falseIndex05_2m] = getTrainIndex(SMTrue05_2m, 1000);
+% [trueIndex05_3m, falseIndex05_3m] = getTrainIndex(SMTrue05_3m, 500);
+% [trueIndex05_5m, falseIndex05_5m] = getTrainIndex(SMTrue05_5m, 300);
+% [trueIndex05_8m, falseIndex05_8m] = getTrainIndex(SMTrue05_8m, 150);
+% [trueIndex05_10m, falseIndex05_10m] = getTrainIndex(SMTrue05_10m, 120);
+
+% 06
+% [trueIndex06_1m, falseIndex06_1m] = getTrainIndex(SMTrue06_1m, 600);
+% [trueIndex06_2m, falseIndex06_2m] = getTrainIndex(SMTrue06_2m, 300);
+% [trueIndex06_3m, falseIndex06_3m] = getTrainIndex(SMTrue06_3m, 180);
+% [trueIndex06_5m, falseIndex06_5m] = getTrainIndex(SMTrue06_5m, 120);
+% [trueIndex06_8m, falseIndex06_8m] = getTrainIndex(SMTrue06_8m, 70);
+% [trueIndex06_10m, falseIndex06_10m] = getTrainIndex(SMTrue06_10m, 60);
+
+% 08
+% [trueIndex08_1m, falseIndex08_1m] = getTrainIndex(SMTrue08_1m, 15000);
+% [trueIndex08_2m, falseIndex08_2m] = getTrainIndex(SMTrue08_2m, 5000);
+% [trueIndex08_3m, falseIndex08_3m] = getTrainIndex(SMTrue08_3m, 2000);
+% [trueIndex08_5m, falseIndex08_5m] = getTrainIndex(SMTrue08_5m, 1100);
+% [trueIndex08_8m, falseIndex08_8m] = getTrainIndex(SMTrue08_8m, 600);
+% [trueIndex08_10m, falseIndex08_10m] = getTrainIndex(SMTrue08_10m, 400);
